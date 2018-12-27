4b02c255165413c7876855fd80c2d5159954ed8b
Kw Huang <kuanweih@andrew.cmu.edu>
2018-12-27 11:37:25 +0800

    encoded t1 to part script

--- mmbh_func.py
+++ mmbh_func.py
@@ -6,6 +6,7 @@ BOXSIZE = 15.    # Mpc/h
 HUBBLE = 0.697
 TOMSUN = 1e10 / HUBBLE
 
+""" mass funcstions """
 N_BIN = 30
 HALO_MIN = 1e8
 HALO_MAX = 1e13
@@ -13,6 +14,10 @@ BH_MIN = 1e3
 BH_MAX = 1e9
 STAR_MIN = 1e5
 STAR_MAX = 1e12
+
+""" t1 calculations"""
+NMESHS = [5, 15]    # BOXSIZE / scales
+EPSILON = 1e-40    # deal with ki==0.
 
 
 def create_dir(dir_name):
